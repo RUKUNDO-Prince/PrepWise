144e2fbdc7414d8f0eef060d13268ef237345a38
RUKUNDO Prince <rukundoprince951@gmail.com>
2025-04-24 09:30:44 +0200

    some minor changes

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { ReactNode } from "react";
+import { redirect } from "next/navigation";
+import { isAuthenticated } from "@/lib/actions/auth.action";
 
 const RootLayout = async ({ children }: { children: ReactNode }) => {
   const isUserAuthenticated = await isAuthenticated();
